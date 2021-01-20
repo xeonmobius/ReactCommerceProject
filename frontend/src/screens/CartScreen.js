40c68d4313b6774fbcb36935597a4420b8a53332
xeonmobius <refinedzeta@gmail.com>
2021-01-19 22:32:58 -0500

    added remove function to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
 	Button,
 	Card,
 } from 'react-bootstrap';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
 	const productId = match.params.id;
@@ -30,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
 	}, [dispatch, productId, qty]);
 
 	const removeFromCartHandler = (id) => {
-		console.log('Removed');
+		dispatch(removeFromCart(id));
 	};
 
 	const checkoutHandler = () => {
