bfd3226b5602b858de237ea50baeb29418ea9eee
xeonmobius <refinedzeta@gmail.com>
2021-01-25 17:05:08 -0500

    added logout function

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -23,17 +23,21 @@ export const login = (email, password) => async (dispatch) => {
 			config
 		);
 
-        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-        
+		dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
 		localStorage.setItem('userInfo', JSON.stringify(data));
-		
 	} catch (error) {
-        dispatch({
+		dispatch({
 			type: USER_LOGIN_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
 					: error.message,
 		});
-    }
+	}
+};
+
+export const logout = () => (dispatch) => {
+	localStorage.removeItem('userInfo');
+	dispatch({ type: USER_LOGOUT });
 };
